ecc0d6f003bcd4207faaa7ba154801efbb5b82a0
Prasad Dhame <130738670+PRASAD7719@users.noreply.github.com>
2023-12-30 10:18:17 +0530

    Add files via upload

---
+++ Day16FindReplaceofString.java
@@ -0,0 +1,14 @@
+package main1;
+
+public class Day16FindReplaceofString {
+	public static void main(String[] args) {
+		
+		String s="Hi How are you";
+		
+		char c='-';
+		s=s.replace(' ', c);
+		System.out.println("String after replacing spaces");
+		System.out.println(s);
+	}
+
+}
